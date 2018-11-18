2cbf5da350479c7a502574372c4bb4c19c1cf3be
cekogha <vekogha@yahoo.com>
2018-11-18 14:11:56 +0100

    new print out added 3 + (-10) in App

--- src/main/java/fr/cekogha/jenkinsjava/App.java
+++ src/main/java/fr/cekogha/jenkinsjava/App.java
@@ -8,6 +8,7 @@ public class App
 	{
 		    	MathOperations mO = new MathOperations();
 		    	System.out.println("3 + 4 = " + mO.addition(3, 4));
+		    	System.out.println("3 + (-10) = " + mO.addition(3, -10));
 		    	System.out.println("6 - 3 = " + mO.substraction(6, 3));
 		    	System.out.println("6 - 7 = " + mO.substraction(6, 7));
 	}
